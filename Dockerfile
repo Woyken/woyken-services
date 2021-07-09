20c4d19b88d3ba18d150e5b0cd665ed9d7e398b0
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2021-07-10 00:20:39 +0300

    Different startup files for each server

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,6 @@ RUN apk add nodejs
 RUN apk add npm
 COPY . /src
 RUN ["chmod", "+x", "/src/docker_startup.sh"]
-RUN cd /src && npm install
 COPY nginx.conf.template /etc/nginx/conf.d/default.conf
 ENTRYPOINT []
 CMD sed -i -e 's/NGINX_PORT/'"$PORT"'/g' /etc/nginx/conf.d/default.conf && /src/docker_startup.sh && nginx -g 'daemon off;'
--- docker_startup.sh
+++ docker_startup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# Detached run of servers
-node /src/server.js &
-
-node /src/server2.js &
+# Find all servers startup scripts and run them detached
+find /src -name "startup.sh" -print0 | while read -d $'\0' file
+do
+    chmod +x $file
+    # Set working dir to this file location
+    location=`dirname $file`
+    cd $location
+    # Actually run the startup script
+    bash $file &
+done
