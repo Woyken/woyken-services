b6c9b3b338cc322dcfdd26abd26131da9c0baab0
Karolis Uzkuraitis <karolis.uzkuraitis+github@gmail.com>
2021-07-09 20:44:14 +0300

    run nginx

--- docker_startup.sh
+++ docker_startup.sh
@@ -4,4 +4,9 @@ sed -i "s|NGINX_PORT|"$PORT"|g" /src/nginx.conf.template
 
 echo replaced nginx port with $PORT
 
-node /src/server.js
+cp /src/nginx.conf.template /etc/nginx/nginx.conf
+
+# Detached run of server
+node /src/server.js &
+
+exec nginx -g 'daemon off;'
